365669e1585fac032ba24eb404513604bf1b4478
MVDW <maxvdwolf@gmail.com>
2024-02-04 20:26:52 +0100

    Create sql.js

---
+++ src/server/sql.js
@@ -0,0 +1,27 @@
+const mysql = require('promise-mysql');
+
+
+const config = require("../config.js");
+
+
+async function initSQL() {
+    try {
+        
+        const config_db = await config.getConfig("database");
+
+        module.exports.sql = await mysql.createConnection({
+            host: config_db.host,
+            user: config_db.username,
+            password: config_db.password,
+            database: config_db.database
+        });
+
+        return true;
+
+
+    } catch {
+        return false;
+    }
+}
+
+module.exports = { initSQL };
