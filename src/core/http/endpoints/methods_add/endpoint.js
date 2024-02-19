bcabe488036927ccc39e3390d60a1a788d523020
MVDW <maxvdwolf@gmail.com>
2024-02-19 17:50:59 +0100

    method_add can now add a customer

--- src/core/http/endpoints/methods_add/endpoint.js
+++ src/core/http/endpoints/methods_add/endpoint.js
@@ -1,29 +1,74 @@
 const config = require("../../../../config.js");
+const stripe = require('../../../stripe.js').stripeAPI();
+
 const sql = require("../../../sql/sql.js").sql;
 
-const stripe = require('stripe')(config.getConfig("service")["stripe_secret"]);
 
-
-async function endpoint(api_request, obj, post){
+async function endpoint(api_request, obj, post) {
+    const stripeAPI = await stripe;
+    obj["data"] = {};
 
     if (api_request[1] == undefined) {
         obj["code"] = "ENDPOINT_METHODS_ADD_NULL";
-        obj["data"] = {};
         return obj;
     }
 
-    const query = await sql.query("SELECT * FROM customers WHERE id=?", api_request[1]);
-
-
-    console.log(query.length);
+    // TODO: add post verification
 
 
 
-    obj["data"] = {};
-    obj["data"]["methods"] =  "This is a template for " + api_request[1];
+    // create customer if not exist
+    const query = await sql.query("SELECT * FROM customers WHERE local_id=?", api_request[1]);
+    let customer_id = "";
+    if (query.length < 1) {
+        const customer = await stripeAPI.customers.create({
+            name: post["name"],
+            email: post["email"],
+            phone: post["phone"],
+            address: {
+                city: post["city"],
+                country: post["country"],
+                country: post["country"],
+                line1: post["line1"],
+                line2: post["line2"],
+                postal_code: post["postal_code"],
+                state: post["state"]
+            }
+        });
+        customer_id = customer.id;
+        try{
+            await sql.query("INSERT INTO customers (local_id, stripe_id) VALUES (?, ?)", [api_request[1], customer_id]);
+
+        } catch(e){
+            console.log(e)
+        }
+        
+    } else {
+        customer_id = query[0]["stripe_id"];
+    }
+
 
 
     return obj;
 
 }
+
+
+
+async function val() {
+    const paymentMethod = await stripe.paymentMethods.create({
+        type: api_request[2],
+        card: {
+            number: '4242424242424242',
+            exp_month: 8,
+            exp_year: 2026,
+            cvc: '314',
+        },
+    });
+}
+
+
+
+
+
 module.exports = { endpoint };
